917b274774376e9c9ccd5d19379dd27aaaeb2d02
chaudharykulsum738-stack <chaudharykulsum738@gmail.com>
2026-01-11 18:58:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,9 +137,6 @@ st.sidebar.image(
     "https://raw.githubusercontent.com/chaudharykulsum738-stack/mental_health_tracker/main/logo.png",
     width=120
 )
-BASE_DIR = Path(__file__).parent
-LOGO = BASE_DIR / "logo.png"
-st.sidebar.image(str(LOGO, width=120))
 st.markdown(
     """
     <style>
